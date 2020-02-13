a9465b6e19bca73ce77f19e278cae04730407bc5
Abdul Muqsit Baig <muqsitbaig@gmail.com>
2020-02-13 10:03:35 +0530

    Create MyLocalIPAddress.java

---
+++ NP/Java_programs/05_02/MyLocalIPAddress.java
@@ -0,0 +1,11 @@
+import java.net.*;
+
+public class MyLocalIPAddress{
+public static void main(String[] args){
+try{
+InetAddress address = InetAddress.getLocalHost();
+System.out.println(address);
+}
+catch(UnknownHostException e){System.out.println("Could not find local address!!");}
+}
+}
