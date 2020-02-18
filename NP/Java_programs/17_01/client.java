40598c85ffbcae399064825d12b67e1203784780
Abdul Muqsit Baig <muqsitbaig@gmail.com>
2020-02-18 12:07:35 +0530

    Create client.java

---
+++ NP/Java_programs/17_01/client.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class client{
+public static void main(String[] args){
+try{
+Socket s = new Socket("localhost", 1004);
+DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+dout.writeUTF("Hello Serverrr!");
+dout.flush();
+dout.close();
+s.close();
+}
+catch(Exception e){System.out.println(e);}
+}
+}
+
+
+
+
+
