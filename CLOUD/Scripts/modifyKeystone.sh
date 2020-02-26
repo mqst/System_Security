5b77d9924678e273779ac82478d1d006e44da143
Abdul Muqsit Baig <muqsitbaig@gmail.com>
2020-02-26 10:28:58 +0530

    modify keystone.conf

--- CLOUD/Scripts/modifyKeystone.sh
+++ CLOUD/Scripts/modifyKeystone.sh
@@ -4,7 +4,6 @@ mv /etc/keystone/keystone.conf ~/keystone.backup
 cat ~/keystone.backup | awk '$1 ~ /\<lockout_failure_attempts\>/ {gsub(/2/,"10")} {print}' > /etc/keystone/keystone.conf
 
 ##TO RUN THE SCRIPT:
-# ssh mscss@<IP> 'bash -s' < scriptname.sh
-
+## ssh mscss@<IP> 'bash -s' < scriptname.sh
 
 
