99c422ae2c2c11313c47a1b69ce81cfa5cb7bdba
Abdul Muqsit Baig <muqsitbaig@gmail.com>
2020-02-29 10:08:06 +0530

    Create neutron_logs.sh

---
+++ CLOUD/Scripts/neutron_logs.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+##grep neutron /var/log/syslog | awk '/(DEBUG | WARNING)/{print $0}'
+echo -n "Select\n1.Monitor the live log file.\n2.View the entire log file.\n\n"
+read choice
+
+case $choice in
+
+  1)
+    tail -F /var/log/syslog | grep neutron | awk '/(WARNING | ERROR | error| DEBUG)/{print $0}' | egrep '(ERROR | error)'
+    ;;
+
+  *)
+    echo -n "unknown choice"
+    ;;
+esac
+
+##grep neutron /var/log/syslog | awk '/(WARNING | ERROR | error| DEBUG)/{print $0}' | egrep '(ERROR | error)'
+
